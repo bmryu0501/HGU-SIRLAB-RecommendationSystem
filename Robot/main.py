b2502b74f4a8e841fd44a48079447ecc65a9896e
Recommendation System <21900411@handong.ac.kr>
2022-11-23 16:02:32 +0900

    backup robot

--- Robot/main.py
+++ Robot/main.py
@@ -2,18 +2,84 @@
 # -*- coding: utf-8 -*-
 
 from socket import *
+import os
 
-UID = "001"
-local_ip = "192.168.0.164"
+### Socket Info ###
+local_ip = "192.168.137.1"
+port = 2000
 
-clnt_sock = socket(AF_INET, SOCK_STREAM)
-clnt_sock.connect((local_ip, 8080))
+### User Info ###
+UID = None
+CID = None
+TID = None
 
-print("연결 확인됐습니다.")
-clnt_sock.send((UID+"/start").encode("utf-8"))
+scenario_path = './scenario/'
 
+print('''\
+##################
+# Before Playing #
+##################''')
+'''
+0. Local socket에 접속
+'''
+local_sock = socket(AF_INET, SOCK_STREAM)
+local_sock.connect((local_ip, port))
+print("로컬과의 연결을 확인했습니다.")
+
+'''
+1. UserInfo.txt에서 UID, username 저장
+'''
+f = open("UserInfo.txt", "r")
+info = f.readline()
+info = info.split(",")
+UID = info[1]
+username = info[0]
+f.close()
+
+'''
+2. Local에 "UID start" signal 전송
+'''
+local_sock.send((str(UID) + " start").encode("utf-8"))
 print("메시지를 전송했습니다.")
-data = clnt_sock.recv(1024)
 
-print("받은 데이터 : ", data.decode("utf-8"))
-clnt_sock.close()
+'''
+3. Local에서 "choice" 들어오면 로봇이 "하고 싶은 놀이를 춤추는 유령 놀이랑 빨래집게 놀이 중에 골라줘. 다 고른 후에 고른 놀이를 나에게 말해줘" 실행
+'''
+data = local_sock.recv(1024)
+message = data.decode("utf-8")
+if message == "choice":
+    os.system("mpg321 choice.wav")
+
+'''
+4. Receive TID from Local
+'''
+data = local_sock.recv(1024)
+TID = data.decode("utf-8")
+
+print('''\
+##################
+#### Playing #####
+##################''')
+
+'''
+시나리오 실행
+'''
+
+task_path = scenario_path + 'scenario'+TID
+
+# from import or os.chdir() 이동
+
+
+print('''\
+##################
+# After Playing ##
+##################''')
+'''
+1. send "end" signal to local
+'''
+
+
+'''
+2. socket close()
+'''
+local_sock.close()
--- Robot/mic_check.py
+++
@@ -1,9 +0,0 @@
-import pyaudio
-
-po = pyaudio.PyAudio()
-
-for index in range(po.get_device_count()): 
-
-    desc = po.get_device_info_by_index(index)
-
-    print(desc["name"], index, int(desc["defaultSampleRate"]))
